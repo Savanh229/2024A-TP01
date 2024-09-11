45c2a2ad1fe5df038554452789bf763135ef4e49
savanh phengsai <you@example.com>
2024-09-11 17:30:13 -0400

    exo_3

--- ex1.py
+++ ex1.py
@@ -1,5 +1,10 @@
 # TODO: Créer un script permettant le formattage du livre des records des JO.
 
 country = input("De quelle nationalité est l'athlète ? ")
-athlete = ...
-date = ...
+athlete = input("Quel est le nom de l'athlète?")
+date = input("Quel est la date du record?")
+discipline= input("Quel est la discipline?")
+category= input("Quelle est la catégorie?")
+record= input("Quel est le nouveau record?")
+
+print(date, athlete,country, discipline, category, record )
--- ex3.py
+++ ex3.py
@@ -1,5 +1,12 @@
 # TODO: Calculer la distance qu'un poids peut atteindre en utilisant la formule de la portée d'un projectile.
 # TODO: Importer les modules nécessaires.
+import math
 
-speed = ... 
-angle = ...
+speed = float(input("Quelle est la vitesse du projectile en m/s?"))
+angle = float(input("Quelle est l'angle du projectile en degrés ?"))
+x=speed
+y=angle
+g=9.8
+portee=round((pow(x,2)*math.sin(math.radians(2*y)))/g,2)
+
+print("La portée du projectile est de ",portee, "mètres")
